284ee934ec8198fc872bb0be3c155c43e787b031
Mitch Downey <mitchldowney@gmail.com>
2023-12-12 03:34:13 -0600

    Update aws.ts

--- src/factories/aws.ts
+++ src/factories/aws.ts
@@ -1,8 +1,12 @@
-import { AWSService } from 'podverse-external-services'
+import { AWSS3Service, AWSSQSService } from 'podverse-external-services'
 import { config } from '../config'
 
-export const awsInstance = new AWSService({
+const awsConfig = {
   accessKeyId: config.aws?.accessKeyId,
   region: config.aws?.region,
   secretAccessKey: config.aws?.secretAccessKey
-})
+}
+
+export const awsSQSInstance = new AWSSQSService(awsConfig)
+
+export const awsS3Instance = new AWSS3Service(awsConfig)
