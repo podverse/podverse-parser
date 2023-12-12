01696ef5f4ee09ca8ab2a669a1a7d1ae12eef023
Mitch Downey <mitchldowney@gmail.com>
2023-12-12 03:33:42 -0600

    Update compat.ts

--- src/services/partytime/compat.ts
+++ src/services/partytime/compat.ts
@@ -1,20 +1,87 @@
-import { Episode, FeedObject, Phase1Funding, Phase4Medium, Phase4Value } from "podcast-partytime"
+import { Episode as PartytimeEpisode, FeedObject, Phase1Funding, Phase4Medium, Phase4Value } from "podcast-partytime"
 import { Phase4PodcastLiveItem } from 'podcast-partytime/dist/parser/phase/phase-4'
 import { PhasePendingChat } from 'podcast-partytime/dist/parser/phase/phase-pending'
-import { Funding, ParsedEpisode, ParsedLiveItem, ValueTagOriginal, podcastItunesTypeDefaultValue } from "podverse-shared"
+import {
+  Episode,
+  Funding,
+  LiveItemStatus,
+  Podcast,
+  podcastItunesTypeDefaultValue,
+  ValueTagOriginal
+} from "podverse-shared"
 
 /*
   The compat functions convert the podcast-partytime schema
-  to the podverse schema.
+  into the podverse schema, but without the properties that are
+  only relevant for podcast's saved in our database.
 */
 
-// TODO: can we get rid of these interfaces?
-interface ExtendedEpisode extends Episode {
-  contentLinks?: any
-  socialInteraction?: any
+export interface ParsedPodcast extends Pick<
+  Podcast,
+  'podcastGuid'
+  | 'description'
+  | 'funding'
+  | 'imageUrl'
+  | 'isExplicit'
+  | 'itunesFeedType'
+  | 'language'
+  | 'linkUrl'
+  | 'medium'
+  | 'subtitle'
+  | 'title'
+  | 'type'
+  | 'value'
+> {
+  ptAuthors: string[]
+  ptCategories?: string[]
 }
 
-interface ExtendedPhase4PodcastLiveItem extends Phase4PodcastLiveItem {
+export interface ParsedEpisode extends Pick<
+  Episode,
+  'guid'
+  | 'alternateEnclosures'
+  | 'chaptersUrl'
+  // | 'contentLinks' TODO:
+  | 'description'
+  | 'duration'
+  // | 'funding' TODO:
+  | 'imageUrl'
+  | 'isExplicit'
+  | 'itunesEpisode'
+  | 'itunesEpisodeType'
+  | 'itunesSeason'
+  | 'linkUrl'
+  | 'mediaType'
+  | 'mediaUrl'
+  | 'pubDate'
+  | 'socialInteraction'
+  | 'subtitle'
+  | 'title'
+  | 'transcript'
+  | 'value'
+> {
+  // TODO: why any[] instead of Transcript[]?
+  transcript: any[]
+  ptAuthors: any[]
+  ptChapters?: any
+  ptSoundbites: any[]
+}
+
+export type ParsedLiveItem = {
+  chat?: string
+  episode: ParsedEpisode
+  end?: Date | null
+  start?: Date
+  status?: LiveItemStatus
+}
+
+type ParsedPodcastAndLiveItems = {
+  podcast: ParsedPodcast
+  liveItems: ParsedLiveItem[]
+}
+
+// TODO: why are these "extended" interfaces necessary?
+interface ExtendedPartytimeLiveItem extends Phase4PodcastLiveItem {
   chat?: PhasePendingChat
   image?: string
 }
@@ -25,7 +92,89 @@ interface ExtendedChat extends Omit<PhasePendingChat, 'phase'> {
   url?: string
 }
 
-export const fundingCompat = (funding: Phase1Funding): Funding => {
+export const podcastAndLiveItemCompat = (feed: FeedObject): ParsedPodcastAndLiveItems => {
+  return {
+    podcast: {
+      podcastGuid: feed.guid,
+      description: feed.description,
+      funding: Array.isArray(feed.podcastFunding) ? feed.podcastFunding?.map((f) => fundingCompat(f)) : [],
+      imageUrl: feed.itunesImage || feed.image?.url,
+      isExplicit: feed.explicit,
+      itunesFeedType: feed.itunesType || podcastItunesTypeDefaultValue,
+      language: feed.language,
+      linkUrl: feed.link,
+      medium: feed.medium ?? Phase4Medium.Podcast,
+      subtitle: feed.subtitle,
+      title: feed.title,
+      type: feed.itunesType,
+      value: feed.value ? [valueCompat(feed.value)] : [],
+      ptAuthors: Array.isArray(feed.author) ? feed.author : feed.author ? [feed.author] : [],
+      ptCategories: feed.itunesCategory,
+    },
+    liveItems: feed?.podcastLiveItems?.map((x: any) => liveItemCompatToEpisode(x)) ?? []
+  }
+}
+
+export const episodeCompat = (episode: PartytimeEpisode): ParsedEpisode => {
+  return {
+    guid: episode.guid,
+    alternateEnclosures: episode.alternativeEnclosures ?? [],
+    // TODO: why does contentLinks exist on liveItem but not episode type?
+    // contentLinks: episode.contentLinks || [],
+    description: getLongerSummary(episode.content, episode.description),
+    duration: episode.duration,
+    // TODO: episode.podcastFunding does not exist in partytime
+    // funding: Array.isArray(episode.podcastFunding) ? episode.podcastFunding?.map((f) => fundingCompat(f)) : [],
+    imageUrl: episode.image,
+    isExplicit: episode.explicit,
+    itunesEpisode: episode.podcastEpisode?.number || episode.itunesEpisode,
+    itunesEpisodeType: episode.itunesEpisodeType,
+    itunesSeason: episode.podcastSeason?.number || episode.itunesSeason,
+    linkUrl: episode.link,
+    mediaType: episode.enclosure.type,
+    mediaUrl: episode.enclosure.url,
+    pubDate: episode.pubDate,
+    socialInteraction: episode.podcastSocialInteraction ?? [],
+    subtitle: episode.subtitle,
+    title: episode.title,
+    transcript: episode.podcastTranscripts ?? [],
+    value: episode.value ? [valueCompat(episode.value)] : [],
+    ptAuthors: [episode.author],
+    ptChapters: episode.podcastChapters,
+    ptSoundbites: episode.podcastSoundbites ?? []
+  }
+}
+
+export const liveItemCompatToEpisode = (liveItem: ExtendedPartytimeLiveItem): ParsedLiveItem => {
+  const getChatEmbedUrlValue = (chat?: ExtendedChat) => {
+    if (chat?.phase === 'pending' && chat.embedUrl) {
+      return chat.embedUrl
+    }
+    // deprecated embed value
+    else if (chat?.phase === '4' && chat.url) {
+      return chat.url
+    }
+    return ''
+  }
+
+  return {
+    chat: getChatEmbedUrlValue(liveItem.chat),
+    end: liveItem.end,
+    episode: episodeCompat({
+      ...liveItem,
+      // TODO: why is contentLinks broken?
+      // contentLinks: liveItem.contentLinks,
+      duration: 0,
+      explicit: false,
+      // socialInteraction: []
+    }),
+    start: liveItem.start,
+    // TODO: does partytime already convert toLowerCase?
+    status: liveItem.status?.toLowerCase() as LiveItemStatus
+  }
+}
+
+const fundingCompat = (funding: Phase1Funding): Funding => {
   return {
     value: funding.message,
     url: funding.url
@@ -53,92 +202,6 @@ const valueCompat = (val: Phase4Value): ValueTagOriginal => {
   }
 }
 
-export const podcastCompat = (feed: FeedObject) => {
-  return {
-    author: Array.isArray(feed.author) ? feed.author : feed.author ? [feed.author] : [],
-    blocked: feed.itunesBlock,
-    categories: feed.itunesCategory,
-    description: feed.description,
-    explicit: feed.explicit,
-    funding: Array.isArray(feed.podcastFunding) ? feed.podcastFunding?.map((f) => fundingCompat(f)) : [],
-    generator: feed.generator,
-    guid: feed.guid,
-    imageURL: feed.itunesImage || feed.image?.url,
-    itunesType: feed.itunesType || podcastItunesTypeDefaultValue,
-    language: feed.language,
-    lastBuildDate: feed.lastBuildDate,
-    link: feed.link,
-    liveItems: feed.podcastLiveItems ?? [] as ParsedEpisode[],
-    medium: feed.medium ?? Phase4Medium.Podcast,
-    owner: feed.owner,
-    pubDate: feed.pubDate,
-    subtitle: feed.subtitle,
-    summary: feed.summary,
-    title: feed.title,
-    type: feed.itunesType,
-    value: feed.value ? [valueCompat(feed.value)] : []
-  }
-}
-
-// Convert the podcast-partytime schema to a podverse compatible schema.
-export const episodeCompat = (episode: ExtendedEpisode) => {
-  return {
-    alternateEnclosures: episode.alternativeEnclosures ?? [],
-    author: [episode.author],
-    chapters: episode.podcastChapters,
-    // TODO: why does contentLinks exist on liveItem but not episode type?
-    contentLinks: episode.contentLinks || [],
-    description: episode.content || episode.description,
-    duration: episode.duration,
-    enclosure: episode.enclosure,
-    explicit: episode.explicit,
-    // funding: Array.isArray(episode.podcastFunding) ? episode.podcastFunding?.map((f) => fundingCompat(f)) : [],
-    guid: episode.guid,
-    imageURL: episode.image,
-    itunesEpisode: episode.podcastEpisode?.number || episode.itunesEpisode,
-    itunesEpisodeType: episode.itunesEpisodeType,
-    itunesSeason: episode.podcastSeason?.number || episode.itunesSeason,
-    link: episode.link,
-    pubDate: episode.pubDate,
-    socialInteraction: episode.podcastSocialInteraction ?? [],
-    soundbite: episode.podcastSoundbites ?? [],
-    subtitle: episode.subtitle,
-    summary: getLongerSummary(episode.content, episode.description),
-    title: episode.title,
-    transcript: episode.podcastTranscripts ?? [],
-    value: episode.value ? [valueCompat(episode.value)] : []
-  } as ParsedEpisode
-}
-
-export const liveItemCompatToEpisode = (liveItem: ExtendedPhase4PodcastLiveItem) => {
-  const getChatEmbedUrlValue = (chat?: ExtendedChat) => {
-    if (chat?.phase === 'pending' && chat.embedUrl) {
-      return chat.embedUrl
-    }
-    // deprecated embed value
-    else if (chat?.phase === '4' && chat.url) {
-      return chat.url
-    }
-    return ''
-  }
-
-  return {
-    chat: getChatEmbedUrlValue(liveItem.chat),
-    end: liveItem.end,
-    episode: episodeCompat({
-      ...liveItem,
-      contentLinks: liveItem.contentLinks, // TODO:
-      duration: 0,
-      explicit: false, // TODO: liveItem.explicit
-      socialInteraction: [],
-      subtitle: '', // TODO: liveItem.subtitle
-      summary: '', // TODO: liveItem.summary
-    }),
-    start: liveItem.start,
-    status: liveItem.status?.toLowerCase()
-  } as ParsedLiveItem
-}
-
 // Whichever summary is longer we are assuming is the "full summary" and
 // assigning to the summary column.
 const getLongerSummary = (content?: string, description?: string) => {
@@ -146,4 +209,48 @@ const getLongerSummary = (content?: string, description?: string) => {
   const descriptionLength = description ? description.length : 0
   const longerSummary = contentLength >= descriptionLength ? content : description
   return longerSummary
+}
+
+export const getLatestLiveItemStatus = (parsedLiveItems: ParsedLiveItem[]) => {
+  let latestLiveItemStatus = 'none' as LiveItemStatus
+  for (const parsedLiveItem of parsedLiveItems) {
+    const liveItemStatus = parsedLiveItem.status?.toLowerCase()
+    if (liveItemStatus === 'live') {
+      latestLiveItemStatus = 'live'
+      break
+    } else if (
+      liveItemStatus === 'pending'
+      && latestLiveItemStatus !== 'live'
+    ) {
+      latestLiveItemStatus = 'pending'
+    } else if (
+      liveItemStatus === 'ended'
+      && latestLiveItemStatus !== 'live'
+      && latestLiveItemStatus !== 'pending'
+    ) {
+      latestLiveItemStatus = 'ended'
+    }
+  }
+  return latestLiveItemStatus
+}
+
+export const getLatestLiveItemInfo = (parsedLiveItems: ParsedLiveItem[]) => {
+  let liveItemLatestPubDate = null
+  let liveItemLatestTitle = ''
+  let liveItemLatestImageUrl = ''
+  for (const parsedLiveItem of parsedLiveItems) {
+    const liveItemStatus = parsedLiveItem.status?.toLowerCase()
+    if (
+      liveItemStatus === 'live'
+      && (
+        !liveItemLatestPubDate
+        || new Date(parsedLiveItem.start as any) > new Date(liveItemLatestPubDate)
+      )
+    ) {
+      liveItemLatestPubDate = parsedLiveItem.start
+      liveItemLatestTitle = parsedLiveItem.episode.title || 'Untitled Livestream'
+      liveItemLatestImageUrl = parsedLiveItem.episode.imageUrl || ''
+    }
+  }
+  return { liveItemLatestImageUrl, liveItemLatestPubDate, liveItemLatestTitle }
 }
