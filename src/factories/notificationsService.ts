119279b2010a9e71f34f2b521517a4c7c186f4aa
Mitch Downey <mitchldowney@gmail.com>
2025-10-05 21:52:18 -0500

    fix: add userAgent to NotificationsService and PodcastIndexService for consistency

--- src/factories/notificationsService.ts
+++ src/factories/notificationsService.ts
@@ -4,6 +4,7 @@ import { loggerService } from './loggerService';
 import { config } from '../config';
 
 export const NotificationsServiceFactory = (googleAuthToken: string) => new NotificationsService({
+  userAgent: config.userAgent,
   googleAuthToken,
   firebaseProjectId: config.firebase.projectId,
   loggerService
