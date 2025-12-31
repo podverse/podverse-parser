f200af84c0157331d523fd4d8d61af0fd9ab2efd
Mitch Downey <mitchldowney@gmail.com>
2025-12-30 18:14:49 -0600

    fix: remove logDir configuration from loggerService initialization

--- src/factories/loggerService.ts
+++ src/factories/loggerService.ts
@@ -2,6 +2,5 @@ import { LoggerService } from 'podverse-helpers/dist/lib/backend/logger';
 import { config } from '../config';
 
 export const loggerService = new LoggerService({
-  logDir: config.log.dir,
   logLevel: config.log.level,
 });
